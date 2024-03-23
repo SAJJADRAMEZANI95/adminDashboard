231fe7b9879f3c87e5c20df02e6d2547fc849299
sajjad ramezani <sajjadramezani780@gmail.com>
2024-03-24 02:49:09 +0430

    WIP:add login to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import styles from "@/styles/Home.module.css";
 import Dashboard from "./dashboard";
 import Header from "@/components/header";
 import SideMenu from "@/components/sideMenu";
-import Login from "@/components/login/Login";
+import { useSession } from "next-auth/react";
+import Login from "@/components/login";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -16,8 +18,12 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <Header />
-        <SideMenu />
-        <Dashboard />
+        {session && (
+          <>
+            <SideMenu />
+            <Dashboard />
+          </>
+        )}
         <Login/>
       </main>
     </>
